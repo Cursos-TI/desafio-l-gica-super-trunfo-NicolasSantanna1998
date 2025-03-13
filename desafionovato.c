c1f75983406b3ac90762bae07194a626be33ac0b
NicolasSantanna1998 <nicolascaputto32@gmail.com>
2025-03-13 21:55:51 +0000

    desafio novato 1

---
+++ desafionovato.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    printf("***super trunfo***\n");
+
+   
+    int populacao;
+    float area;
+    float pib;
+    int numerosdepontosturisticos;
+
+
+    printf("insira a populaçao:\n");
+    scanf("%d" , &populacao);
+    printf("insira a area:\n");
+    scanf("%f" , &area);
+    printf("insira o pib:\n");
+    scanf("%f" , &pib);
+    printf("insira numero de pontos turisticos");
+    scanf("%d" , &numerosdepontosturisticos);
+
+
+    
+}
---
+++ supertrunfo.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+   int player1 , player2;
+
+
+   printf("Digite um numero 1 para os dados de sao paulo:\n");
+   scanf("%d" , &player1);
+   
+
+   printf("Carta 1: \n");
+   printf("Estado: A\n");
+   printf("Código: S01\n");
+   printf("Nome da Cidade: São Paulo\n");
+   printf("População: 12325000\n");
+   printf("Área: 1521.11 km²\n");
+   printf("PIB: 699.28 bilhões de reais\n");
+   printf("Número de Pontos Turísticos: 50\n");
+   
+   printf("Digite um numero 2 para dados rio de janeiro:\n");
+   scanf("%d" , &player2);
+
+   printf("Carta 2:\n");
+   printf("Estado: B\n");
+   printf("Código: R02\n");
+   printf("Nome da Cidade: Rio de Janeiro\n");
+   printf("População: 6748000\n");
+   printf("Área: 1200.25 km²\n");
+   printf("PIB: 300.50 bilhões de reais\n");
+   printf("Número de Pontos Turísticos: 30 \n");
+
+
+
+}
